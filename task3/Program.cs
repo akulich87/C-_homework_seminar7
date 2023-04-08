3a056b45908ebe5c10c0831b9db39193d5eef47d
akulich87 <amgrupp_project@mail.ru>
2023-04-08 22:45:27 +0300

    add task3

--- task3/Program.cs
+++ task3/Program.cs
@@ -16,7 +16,7 @@ for (int i = 0; i < array.GetLength(0); i++)
   for (int j = 0; j < array.GetLength(1); j++) 
   {
     array[i,j] = new Random().Next(10);
-    Console.Write(array[i,j]+" "); 
+    Console.Write(array[i,j] +" "); 
   }
   Console.WriteLine(); 
 }
@@ -24,6 +24,7 @@ for (int i = 0; i < array.GetLength(0); i++)
 Console.WriteLine();
 
 Console.Write("Среднее арифметическое каждого столбца: ");
+
 for (int j = 0; j < array.GetLength(1); j++) 
 {
   double sum = 0;
